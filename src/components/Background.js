8f908080811ed3bb5f5b7d9dad3635b22fdbef6a
Christophe Beveraggi <christophe@beve.org>
2020-03-07 16:59:46 +0100

    Fix background flickering

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,7 +4,7 @@ import { a } from '@react-spring/three'
 const Background = ({ color }) => {
 
   return (
-    <mesh scale={[2000, 2000, 1]} position={[0,0,-20]} fog={false}>
+    <mesh scale={[100, 100, 1]} position={[0,0,-10]} fog={false}>
       <planeGeometry attach="geometry" args={[1, 1]} />
       <a.meshBasicMaterial attach="material" color={color} depthTest={false} fog={false} />
     </mesh>
