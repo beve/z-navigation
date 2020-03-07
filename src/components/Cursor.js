6bcb335ae59bfcf529155ad596f9895d4db452e8
Christophe Beveraggi <christophe@beve.org>
2020-03-07 11:12:03 +0100

    SVG Cursors (wip)

--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -1,32 +1,38 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState, useContext, useMemo, useCallback } from 'react'
 import SVG from './SVG'
-import lerp from 'lerp'
-import * as THREE from 'three'
 import { useFrame, useThree } from 'react-three-fiber'
+import { useTransition } from '@react-spring/three'
+import { StateContext } from './AnimationContext'
 
 const Cursor = () => {
 
   const svgRef = useRef()
-  const vect = new THREE.Vector3()
-  const { viewport } = useThree()
 
-  useFrame(({ mouse, camera, viewport }) => {
-    // svgRef.current.position.x = lerp(svgRef.current.position.x, mouse.x, 1)
-    // svgRef.current.position.y = lerp(svgRef.current.position.y, mouse.y, 1)
-    // svgRef.current.position.x = mouse.x
-    // svgRef.current.position.y = mouse.y
-    // const val = -camera.position.z+1;
-    vect.set(mouse.x, mouse.y, camera.position.z-1)
-    vect.unproject(camera)
-    // svgRef.current.position.set(vect);
-    svgRef.current.position.x = vect.x * viewport.width;
-    svgRef.current.position.y = vect.y;
+  const state = useContext(StateContext)
+
+  const icons = {
+    'eye': useCallback(({style}) => <SVG style={{...style}} ref={svgRef} key={'/assets/eye.svg'} src={'/assets/eye.svg'} opacity={1} />, []),
+    'pointer': useCallback(({style}) => <SVG style={{...style}} ref={svgRef} key={'/assets/cursor.svg'} src={'/assets/cursor.svg'} opacity={1} />, []),
+    'close': useCallback(({style}) => <SVG style={{...style}} ref={svgRef} key={'/assets/exit.svg'} src={'/assets/exit.svg'} opacity={1} />, []),
+  }
+
+  const transitions = useTransition(icons[state.cursor], item => item, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
   })
 
+  useFrame(({ mouse, viewport }) => {
+    if (svgRef.current) {
+      svgRef.current.position.x = (mouse.x * viewport.width / 2)
+      svgRef.current.position.y = (mouse.y * viewport.height / 2)
+    }
+  })
 
-  return (
-    <SVG ref={svgRef} src="/assets/cursor.svg" position={[0, 0, 0]} />
-  )
+  return transitions.map(({ item, key, props }) => {
+    const Svg = item
+    return <Svg style={props} key={key} />
+  })
 
 }
 
