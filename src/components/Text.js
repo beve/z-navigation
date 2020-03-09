c2d3b09947b22a010ed806998437c3ad0b06222c
Christophe Beveraggi <christophe@beve.org>
2020-03-09 13:01:34 +0100

    Allow to align text on bottom

--- src/components/Text.js
+++ src/components/Text.js
@@ -2,12 +2,12 @@ import * as THREE from '../three-exports'
 import React, { useState, useMemo } from 'react'
 import fontFile from '../victormono'
 
-export default function Text({ children, size = 1, letterSpacing = 0.01, lineHeight = 0.04, color = '#000000', centerX = true, centerY = true, ...props }) {
+export default function Text({ children, size = 1, letterSpacing = 0.01, lineHeight = 0.07, color = '#000000', centerX = true, centerY = true, verticalAlign = 'top', ...props }) {
   const [font] = useState(() => new THREE.FontLoader().parse(fontFile))
   const [shapes, [x, y]] = useMemo(() => {
     let x = 0,
       y = 0,
-      lineNum = 1
+      lineNum = 0
     let letters = [...String(children)]
     let mat = new THREE.MeshBasicMaterial({ color, opacity: 1, transparent: true })
     let matT = new THREE.MeshBasicMaterial({ color, opacity: 0, transparent: true })
@@ -23,16 +23,16 @@ export default function Text({ children, size = 1, letterSpacing = 0.01, lineHei
           x += geom.boundingBox.max.x + letterSpacing
         }
         mesh.position.x = x
-        y = Math.max(y, geom.boundingBox.max.y+lineHeight)
-        mesh.position.y = y*-lineNum
+        y = Math.max(y, geom.boundingBox.max.y)
+        mesh.position.y = lineNum ? ((y+lineHeight)*-lineNum) : y*-lineNum
         return mesh
       }),
-      [x, y]
+      [x, verticalAlign === 'top' ? y : lineNum ? ((y+(lineHeight))*-(lineNum-1)) : y]
     ]
   }, [font, children, size, letterSpacing, color])
   return (
     <group {...props}>
-      <group position={[centerX ? -x / 2 : 0, centerY ? -y / 2 : 0, 0]}>
+      <group position={[centerX ? -x / 2 : 0, centerY ? -y / 2 : -y, 0]}>
         {shapes.map((shape, index) => (
           <primitive key={index} object={shape} />
         ))}
